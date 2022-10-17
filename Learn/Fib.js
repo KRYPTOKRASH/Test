4e6d58950e444ed3a07d73ffc1ecd4ce510eca78
KRYPTOKRASH <maxbaga_1506@mail.ru>
2022-10-17 21:57:29 +0800

    proto

--- Learn/Fib.js
+++ Learn/Fib.js
@@ -10,3 +10,16 @@ function fib(n) {
 }
 
 console.log(fib(9))
+
+function fib(n) {
+    let fInt = 1;
+    let sInt = 1;
+    for (let i = 3; i <= n; i++) {
+        let fibInt = fInt + sInt
+        fInt = sInt
+        sInt = fibInt
+    }
+    return sInt
+}
+
+console.log(fib(9))
